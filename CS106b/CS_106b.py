60e01adbab9cb960204d4a132667b031763b94df
thisiswei <99plus2@gmail.com>
2014-03-30 18:07:16 -0400

    programming exercises from cs106b chapter 05

---
+++ CS106b/CS_106b.py
@@ -0,0 +1,34 @@
+def digit_sum(n):
+    def helper(n):
+        if n < 10:
+            return n
+        p, r = divmod(n, 10)
+        return r + helper(p)
+    return helper(n)
+
+def digit_root(n):
+    def helper(n, steps):
+        if n < 10:
+            return steps
+        return helper(digit_sum(n), steps+1)
+    return helper(n, 0)
+
+def C(n, k):
+    if n == 0 or k == 0 or n == k:
+        return 1
+    return C(n-1, k-1) + C(n-1, k)
+
+def reverse_string(s):
+    if len(s) == 1:
+        return s
+    return reverse_string(s[1:]) + s[0:1]
+
+def test():
+    assert digit_sum(1729) == 19
+    assert digit_root(1729) == 3
+    assert C(6, 2) == 15
+    assert reverse_string('string') == 'gnirts'
+    print 'pass'
+
+if __name__ == '__main__':
+    exit(test())
