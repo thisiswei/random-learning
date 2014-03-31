fdde97370345ac91ea2394201519dd7de28d3ae3
thisiswei <99plus2@gmail.com>
2014-03-31 17:25:40 -0400

    tower

--- CS106b/06.py
+++ CS106b/06.py
@@ -1,3 +1,17 @@
+"""
+If the recursive decomposition doesn’t end up violating the rules, everything
+should be okay.
+
+The first rule—that only one disk can be moved at a time—is not an
+issue. If there is more than a single disk, the recursive decomposition breaks the problem
+down to generate a simpler case. The steps in the pseudocode that actually transfer disks
+move only one disk at a time.
+
+The second rule—that you are not allowed to place a larger
+disk on top of a smaller one—is the critical one. You need to convince yourself that you
+will not violate this rule in the recursive decomposition.
+"""
+
 #Hanoi move tower
 def move(size, start='L', using='M', end='R'):
     if len(size) == 1:
