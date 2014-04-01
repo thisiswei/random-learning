f7804a10afd892dcd0e8adfefc4422377b1d7756
thisiswei <99plus2@gmail.com>
2014-03-31 23:04:52 -0400

    mnemonics

--- CS106b/06.py
+++ CS106b/06.py
@@ -1,16 +1,7 @@
 """
-If the recursive decomposition doesn’t end up violating the rules, everything
-should be okay.
-
-The first rule—that only one disk can be moved at a time—is not an
-issue. If there is more than a single disk, the recursive decomposition breaks the problem
-down to generate a simpler case. The steps in the pseudocode that actually transfer disks
-move only one disk at a time.
-
-The second rule—that you are not allowed to place a larger
-disk on top of a smaller one—is the critical one. You need to convince yourself that you
-will not violate this rule in the recursive decomposition.
+http://cs.stanford.edu/people/eroberts/courses/cs106b/chapters/06-recursive-procedures.pdf
 """
+
 
 #Hanoi move tower
 def move(size, start='L', using='M', end='R'):
@@ -18,8 +9,60 @@ def move(size, start='L', using='M', end='R'):
         return [size[0] + ',' + start + '->' + end]
     return move(size[:-1], start, end, using) + move(size[-1], start, using, end) + move(size[:-1], using, start, end)
 
-print move('123')
+def permutations(ls):
+    if ls == []:
+        return [[]]
+    head = ls[0:1]
+    return [s[:i] + head + s[i:] for s in permutations(ls[1:]) for i in range(len(s)+1)]
+
+def countmoves(n):
+    if n == 0:
+        return 0
+    return 1 + 2 * countmoves(n-1)
+
+# why the fuck did I make this so complicated
+#import string
+#def get_dict():
+#    letters = string.letters[26:]
+#    ranges = get_start_end(0, 3)
+#    return dict((i, letters[s:e])
+#                for (i, (s, e)) in zip(range(2, 10), ranges))
+#
+#
+#def get_start_end(start, end):
+#    if end == 26:
+#        return [(22, 26)]
+#    return [(start, end)] + get_start_end(end, end+[3, 4][end in (15, 22)])
+
+d = {
+        2: 'ABC',
+        3: 'DEF',
+        4: 'GHI',
+        5: 'JKL',
+        6: 'MNO',
+        7: 'PQRS',
+        8: 'TUV',
+        9: 'WXYZ'
+        }
 
 
+def mnemonics(string):
+    group_letters = [d.get(int(s)) for s in string]
+    def helper(group):
+        if group == []:
+            return ''
+        head, rest = group[0], group[1:]
+        if not rest:
+            return [h for h in head]
+        return [h+g for g in helper(rest) for h in head]
+    return helper(group_letters)
 
+
+def test():
+    assert mnemonics('723') == ['PAD', 'QAD', 'RAD', 'SAD', 'PBD', 'QBD', 'RBD', 'SBD', 'PCD', 'QCD', 'RCD', 'SCD', 'PAE', 'QAE', 'RAE', 'SAE', 'PBE', 'QBE', 'RBE', 'SBE', 'PCE', 'QCE', 'RCE', 'SCE', 'PAF', 'QAF', 'RAF', 'SAF', 'PBF', 'QBF', 'RBF', 'SBF', 'PCF', 'QCF', 'RCF', 'SCF']
+    assert len(mnemonics('723')) == 36
+
+
+if __name__ == '__main__':
+    exit(test())
 
