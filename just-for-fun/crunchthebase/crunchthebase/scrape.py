47df1257d055f4035693fd52d179aba3da6ae9f8
thisiswei <99plus2@gmail.com>
2014-07-03 10:04:33 -0400

    minor

--- just-for-fun/crunchthebase/crunchthebase/scrape.py
+++ just-for-fun/crunchthebase/crunchthebase/scrape.py
@@ -4,24 +4,23 @@ from utils.dicttools import get_dotted
 from settings import APIKEY
 from utils.timeutils import to_datetime
 
-BASE_URL = "http://api.crunchbase.com/v/2/{utility}?order=updated_at%20desc&apikey=" + APIKEY
+BASE_URL = "http://api.crunchbase.com/v/2/{item}?order=updated_at%20desc&user_key=" + APIKEY
 
-CURRENT_IDX = 1
 MAX = 100
 SLEEP = 60
 
 @retry(sleep_interval=60)
-def get_companies():
+def get_companies(max_=2):
     to_return = []
-    url = BASE_URL.format(utility="organizations")
-    for i in range(CURRENT_IDX, MAX):
+    url = BASE_URL.format(item="organizations")
+    for i in range(1, max_):
         cur_url = url + "&page=%d" % i
         json_ = _get_json(cur_url)
         names_and_links = _get_company_name_and_path(json_)
+        import ipdb; ipdb.set_trace()
         for _, permlink in names_and_links:
             company = _get_company(permlink)
             to_return.append(company)
-        CURRENT_IDX += 1
     return to_return
 
 def _create_company_dict(name=None, city=None, total_founding=None,
@@ -31,14 +30,22 @@ def _create_company_dict(name=None, city=None, total_founding=None,
                     external_id=None,
                     desc=None,
                     is_closed=None,
+                    founders=None,
                     ):
-    return dict(name=name, city=city, total_founding=total_founding,
-                category=category, homepage_url=homepage_url, external_id=external_id,
-                desc=desc, founded_on=founded_on, is_closed=is_closed)
+    return dict(name=name,
+                city=city,
+                total_founding=total_founding,
+                category=category,
+                homepage_url=homepage_url,
+                external_id=external_id,
+                desc=desc,
+                founded_on=founded_on,
+                is_closed=is_closed,
+                founders=founders)
 
 def _get_company(permlink):
-    utility = 'organization/%s' % permlink
-    url = BASE_URL.format(utility=utility)
+    item = 'organization/%s' % permlink
+    url = BASE_URL.format(item=item)
     json_ = _get_json(url)
 
     data = json_.get('data')
@@ -48,15 +55,16 @@ def _get_company(permlink):
     name =  properties.get('name')
     desc = properties.get('short_description')
     founded_on = to_datetime(properties.get('founded_on'))
-    total_founding = int()
+    total_founding = properties.get('total_funding_usd')
     homepage_url = properties.get('homepage_url')
     is_closed = properties.get('is_closed')
 
     relationships = data.get('relationships')
+    #addr_items = get_dotted(relationships, 'headquarters.items[0]') #address
     city = get_dotted(relationships, 'headquarters.items[0].city')
     category = get_dotted(relationships, 'categories.items[0].name')
-    founders = [item.get('name')
-                for item in get_dotted(relationships, 'founders.items')]
+    founders = [i.get('name')
+                for i in get_dotted(relationships, 'founders.items')]
 
     dict_ = _create_company_dict(name=name, city=city, total_founding=total_founding,
                     founded_on=founded_on,
@@ -65,6 +73,7 @@ def _get_company(permlink):
                     external_id=external_id,
                     desc=desc,
                     is_closed=is_closed,
+                    founders=founders,
                     )
     return dict_
 
@@ -72,7 +81,7 @@ def _get_json(url):
     resp = requests.get(url)
     if resp.status_code != 200:
         raise ValueError("404")
-    return resp.json()
+    return resp.json
 
 def _get_company_name_and_path(json_):
     def _get_permlink(path):
